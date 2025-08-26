bf0f58f1bdc7bd7fe8484a50bd526b73e3f59209
encypted10 <kmarsalan.com>
2025-08-26 14:51:31 +0530

    comment bash

--- portfolio/build.sh
+++ portfolio/build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # Exit on error
-set -o errexit
+# set -o errexit
 
-pip install --upgrade pip
-pip install -r requirements.txt
-python manage.py collectstatic --noinput
-python manage.py migrate
+# pip install --upgrade pip
+# pip install -r requirements.txt
+# python manage.py collectstatic --noinput
+# python manage.py migrate
